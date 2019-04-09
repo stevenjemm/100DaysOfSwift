17db6fb2bbbec26f9219edc941d209bc64631d55
Steven Jemmott <jemmott.steven@gmail.com>
2019-04-08 23:12:59 -0400

    Add detailViewController to project

---
+++ Project1/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Steven Jemmott on 06/04/2019.
+//  Copyright © 2019 Bottled Sunshine. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    // MARK: - Properties
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
